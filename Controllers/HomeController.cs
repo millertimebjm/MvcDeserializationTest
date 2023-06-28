5721a4bafc07f374531cd216db0f6d5b6d6b86fb
Brandon Miller <bmiller@pesi.com>
2023-06-28 15:04:48 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace MvcDeserializationTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetOrder(Order o)
+        {
+            return Json(o.OrderId);
+        }
+    }
+
+    public class Order
+    {
+        public string OrderId { get; set; }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcDeserializationTest.MvcApplication" Language="C#" %>
